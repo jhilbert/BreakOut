b48f21489e12123039700d9c6023c88f43db11ed
Stephen Compton <sccompton@me.com>
2014-01-16 13:57:25 -0600

    Initial Commit

---
+++ BreakOut/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BreakOut
+//
+//  Created by Stephen Compton on 1/16/14.
+//  Copyright (c) 2014 Stephen Compton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BreakOut/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BreakOut
+//
+//  Created by Stephen Compton on 1/16/14.
+//  Copyright (c) 2014 Stephen Compton. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
